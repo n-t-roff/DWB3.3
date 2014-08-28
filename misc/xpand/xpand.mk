9aaab3a7b266031efaa70c8bd6f6ede72ff91ac9
n-t-roff <troff@arcor.de>
2014-08-28 18:47:03 +0200

    Install xpand to bin instead of lib

--- misc/xpand/xpand.mk
+++ misc/xpand/xpand.mk
@@ -7,7 +7,7 @@ VERSION=3.3
 GROUP=bin
 OWNER=bin
 
-LIBDIR=/usr/lib/dwb
+BINDIR=/usr/bin
 MAN5DIR=/usr/man/p_man/man5
 
 CFLGS=-O
@@ -19,16 +19,16 @@ LDFLAGS=$(LDFLGS)
 all : xpand
 
 install : all
-	@if [ ! -d "$(LIBDIR)" ]; then \
-	    mkdir $(LIBDIR); \
-	    chmod 755 $(LIBDIR); \
-	    chgrp $(GROUP) $(LIBDIR); \
-	    chown $(OWNER) $(LIBDIR); \
+	@if [ ! -d "$(BINDIR)" ]; then \
+	    mkdir $(BINDIR); \
+	    chmod 755 $(BINDIR); \
+	    chgrp $(GROUP) $(BINDIR); \
+	    chown $(OWNER) $(BINDIR); \
 	fi
-	cp xpand $(LIBDIR)
-	@chmod 755 $(LIBDIR)/xpand
-	@chgrp $(GROUP) $(LIBDIR)/xpand
-	@chown $(OWNER) $(LIBDIR)/xpand
+	cp xpand $(BINDIR)
+	@chmod 755 $(BINDIR)/xpand
+	@chgrp $(GROUP) $(BINDIR)/xpand
+	@chown $(OWNER) $(BINDIR)/xpand
 	cp xpand.5 $(MAN5DIR)/xpand.5
 	@chmod 644 $(MAN5DIR)/xpand.5
 	@chgrp $(GROUP) $(MAN5DIR)/xpand.5
@@ -50,12 +50,12 @@ changes :
 	    -e "s'^VERSION=.*'VERSION=$(VERSION)'" \
 	    -e "s'^GROUP=.*'GROUP=$(GROUP)'" \
 	    -e "s'^OWNER=.*'OWNER=$(OWNER)'" \
-	    -e "s'^LIBDIR=.*'LIBDIR=$(LIBDIR)'" \
+	    -e "s'^BINDIR=.*'BINDIR=$(BINDIR)'" \
 	    -e "s'^MAN5DIR=.*'MAN5DIR=$(MAN5DIR)'" \
 	$(MAKEFILE) >X$(MAKEFILE); \
 	mv X$(MAKEFILE) $(MAKEFILE); \
 	sed \
-	    -e "s'^.ds dL.*'.ds dL $(LIBDIR)'" \
+	    -e "s'^.ds dL.*'.ds dL $(BINDIR)'" \
 	xpand.5 >Xxpand.5; \
 	mv Xxpand.5 xpand.5
 
